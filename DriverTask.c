0aeeab1829d49d56dd12af755ea30b09c4a9537f
VodkaBottle <34173944+VodkaBottle@users.noreply.github.com>
2018-06-28 01:30:14 -0400

    Add files via upload

--- DriverTask.c
+++ DriverTask.c
@@ -126,14 +126,64 @@ power=-(p+i+d);
 	//END PIDRight
 }
 
+task flipCap
+{
+int CF=180;
+int CFN=CF;
+	SensorValue[CFlip]=abs(sensorValue[CFlip]);
+	while (sensorValue[CFlip]>CF) sensorValue[CFlip]-=CF;
+	while((CFN-sensorValue[CFlip])>=5) motor[CFlipper]=127;
+	motor[CFlipper]=0;
+	sensorValue[CFlip]-=CF;
+}
+
 task Control
 {
 	//DECLARATIONS
 	int threshold = 5;
-	int Y2=0, X1=0;
+	int Y2=0, X1=0, Y4=0, on=0;
+	while(true)
+	{
+//---------------------------------------------FIRST CONTROLER----------------------------------------------------------
+			//drive
+			Y2=vexRt[Ch2];
+			X1=vexRT[Ch1];
+			if(abs(Y2)<threshold) Y2=0;
+			if(abs(X1)<threshold) X1=0;
+			motor[RDrive]=Y2-X1;
+			motor[RDrive]=Y2+X1;
 
-		while(true){
+			//capflipper
+			if(vexRT[Btn7L]==1){
+				startTask(flipCap);
+			}
+			else if(vexRT[Btn7U]==1){
+				on=1;
+				motor[CFlipper]=127;
+			}
+			else if(vexRT[Btn7D]==1){
+				on=1;
+				motor[CFlipper]=-127;
+			}
+			else if(on==1){
+				on=0;
+				motor[CFlipper]=0;
+			}
 
+			//6-bar lift
+			Y4=vexRT[Ch4];
+			if(abs(Y4)<threshold) Y4=0;
+			motor[LeftBar]=Y4;
+			motor[RightBar]=Y4;
+//---------------------------------------------SECOND CONTROLER----------------------------------------------------------
 
-	}
+			//ball intake
+			if(vexRT[Btn7UXmtr2])
+				motor[BIntake]=127;
+			else if(vexRT[Btn7DXmtr2])
+				motor[BIntake]=-127;
+			else motor[BIntake]=0;
+			//flywheel
+			motor[FlyWheel]=127;
+		}
 }
